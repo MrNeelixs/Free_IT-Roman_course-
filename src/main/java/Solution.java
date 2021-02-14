9914e0d96cdf3f40614579deab4070ee07e583e0
Alexander Malashkevich <mr.neelixs@gmail.com>
2021-02-14 15:02:39 +0300

    Home work 01 Method implementation Main (System.out.println)

---
+++ src/main/java/Solution.java
@@ -0,0 +1,14 @@
+/*
+                        Задание 1
+- Создать Git репозиторий.
+- Создать первый проект. Я создал на базе framework Maven. (version SDK 1.8 ).
+- Реализация первого метода через ( System.out.println ).
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Привет группа FreeIT !");
+        System.out.print("Меня зовут Александр, ");
+        System.out.println("буду рад взаимной помощи в домашних заданиях.");
+        System.out.println("Успехов в освоении Java !!!");
+    }
+}
